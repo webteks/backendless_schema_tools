5f75882e505e729aa8ba5601b8917707420b858b
Ruslan Makarenko <ruslan.makarenko@gmail.com>
2018-07-02 14:20:44 +0300

    get rid of ids in dump file and prettify json (DF-7806)

--- libs/backendless-console-api.js
+++ libs/backendless-console-api.js
@@ -3,9 +3,9 @@
 const _ = require('lodash')
 const axios = require('axios')
 const chalk = require('chalk')
-const {promisify} = require('util')
+const { promisify } = require('util')
 
-let {readFile, writeFile, stat} = require('fs')
+let { readFile, writeFile, stat } = require('fs')
 
 writeFile = promisify(writeFile)
 readFile = promisify(readFile)
@@ -33,7 +33,7 @@ class Backendless {
             baseURL: beURL,
             timeout: timeout,
             headers: {
-                'content-type': 'application/json',
+                'content-type'    : 'application/json',
                 'application-type': 'REST'
             }
         })
@@ -63,10 +63,10 @@ class Backendless {
             clientBaseURL: beURL,
             serverBaseURL,
             appsContext,
-            appList: [],
-            appsToCheck: [],
+            appList      : [],
+            appsToCheck  : [],
             appNamesToCheck,
-            controlApp: {},
+            controlApp   : {},
             controlAppName,
             instance,
             password,
@@ -77,8 +77,8 @@ class Backendless {
     }
 
     /* Build app headers given appId and secretKey */
-    _getAppHeaders({appId, secretKey}) {
-        return {headers: {'application-id': appId, 'secret-key': secretKey}}
+    _getAppHeaders({ appId, secretKey }) {
+        return { headers: { 'application-id': appId, 'secret-key': secretKey } }
     }
 
     /* Build appversion api path provided currentVersionId */
@@ -88,7 +88,7 @@ class Backendless {
 
     /* Authenticate user & add auth-key to header for future requests */
     login() {
-        return this.instance.post('/console/home/login', {'login': this.username, 'password': this.password})
+        return this.instance.post('/console/home/login', { 'login': this.username, 'password': this.password })
             .then(res => this.instance.defaults.headers['auth-key'] = res.headers['auth-key'])
     }
 
@@ -123,14 +123,14 @@ class Backendless {
 
     getAppList() {
         return this.instance.get('/console/applications')
-            .then(({data: appList}) => this.appList.push(...appList))
+            .then(({ data: appList }) => this.appList.push(...appList))
     }
 
     /* Filter application list based on beVersion & which apps are actually needed for checks */
     filterAppList() {
         this.appList = _(this.appList)
             .filter(app => _.includes(this.appsContext, app.name || app.appName))
-            .value();
+            .value()
     }
 
     getAppSecrets() {
@@ -138,7 +138,7 @@ class Backendless {
             filterLive(this.appList).map(app => {
                 if (app.id) {
                     return this.instance.get(`/${app.id}/console/appsettings`)
-                        .then(({data}) => app.secretKey = data.devices.REST);
+                        .then(({ data }) => app.secretKey = data.devices.REST)
                 }
             })
         )
@@ -148,15 +148,15 @@ class Backendless {
         console.log('Fetching schema..')
 
         const normalizeTable = table => {
-            table.columns = table.columns.filter(column => !SYSTEM_COLUMNS.includes(column.name));
+            table.columns = table.columns.filter(column => !SYSTEM_COLUMNS.includes(column.name))
 
-            return table;
+            return table
         }
 
         return Promise.all(
             filterLive(this.appList).map(app => {
                 return this.instance.get(`${this._getConsoleApiUrl(app)}/data/tables`)
-                    .then(({data}) => app.tables = _.sortBy(data.tables, 'name').map(normalizeTable));
+                    .then(({ data }) => app.tables = _.sortBy(data.tables, 'name').map(normalizeTable))
             })
         )
     }
@@ -170,7 +170,7 @@ class Backendless {
 
         return Promise.all(
             filterLive(this.appList).map(app => {
-                return this.getRoles(app.id).then(({data}) => app.roles = data);
+                return this.getRoles(app.id).then(({ data }) => app.roles = data)
             })
         )
     }
@@ -182,7 +182,7 @@ class Backendless {
             filterLive(this.appList).map(app => Promise.all(
                 app.roles.map(role => {
                     return this.instance.get(`${this._getConsoleApiUrl(app)}/security/roles/permissions/${role.roleId}`)
-                        .then(({data}) => role.permissions = data);
+                        .then(({ data }) => role.permissions = data)
                 })
             ))
         )
@@ -196,7 +196,7 @@ class Backendless {
                 return Promise.all(
                     app.tables.map(table => {
                         return this.instance.get(`${this._getConsoleApiUrl(app)}/security/data/${table.tableId}/users`)
-                            .then(({data}) => table.users = data.data)
+                            .then(({ data }) => table.users = data.data)
                     })
                 )
             })
@@ -211,7 +211,7 @@ class Backendless {
                 app.tables.map(table => {
 
                     return this.instance.get(`${this._getConsoleApiUrl(app)}/security/data/${table.tableId}/roles`)
-                        .then(({data}) => table.roles = data)
+                        .then(({ data }) => table.roles = data)
                 }))
             )
         )
@@ -223,12 +223,12 @@ class Backendless {
         return Promise.all(
             filterLive(this.appList).map(async app => {
 
-                return this.instance.get(this._getConsoleApiUrl(app) + '/localservices').then(({data: services}) => {
+                return this.instance.get(this._getConsoleApiUrl(app) + '/localservices').then(({ data: services }) => {
                     app.services = services
 
                     return Promise.all(services.map(service => {
                         return this.instance.get(`${this._getConsoleApiUrl(app)}/localservices/${service.id}/methods`)
-                            .then(({data: methods}) => service.methods = methods)
+                            .then(({ data: methods }) => service.methods = methods)
                     }))
                 })
             })
@@ -245,14 +245,14 @@ class Backendless {
 
                     return this.instance.get(
                         `${this._getConsoleApiUrl(app)}/security/localservices/${service.id}/roles?pageSize=50`)
-                        .then(({data}) => {
+                        .then(({ data }) => {
                             // endpoint permission sync requires roles list
                             if (appIndex > 0) {
                                 service.roles = data
                             }
 
                             data.forEach(role => {
-                                role.permissions.forEach(({operation, access}) => {
+                                role.permissions.forEach(({ operation, access }) => {
                                     const method = methodsMap[operation]
 
                                     method.roles = method.roles || {}
@@ -287,28 +287,28 @@ class Backendless {
 
     /* Set controlApp & appsToCheck from appList and return copy of data */
     getApps() {
-        const findAppByName = appName => _.find(this.appList, {'name': appName});
+        const findAppByName = appName => _.find(this.appList, { 'name': appName })
         const controlApp = findAppByName(this.controlAppName)
 
         if (!controlApp) {
-            throw new Error(`${this.controlAppName} app does not exist`);
+            throw new Error(`${this.controlAppName} app does not exist`)
         }
 
         const appsToCheck = this.appNamesToCheck.map(appName => {
             const app = findAppByName(appName)
 
             if (!app) {
-                throw new Error(`${appName} app does not exist`);
+                throw new Error(`${appName} app does not exist`)
             }
 
-            return app;
+            return app
         })
 
         return [controlApp, ...appsToCheck]
     }
 
     addTable(appId, name) {
-        return this.instance.post(`${appId}/console/data/tables`, {name})
+        return this.instance.post(`${appId}/console/data/tables`, { name })
     }
 
     removeTable(appId, name) {
@@ -324,6 +324,7 @@ class Backendless {
 
         return this.instance.post(path, column)
     }
+
     updateColumn(appId, table, column) {
         const columnName = column.name || column.columnName
         let path = tableColumnsUrl(appId, table)
@@ -335,6 +336,7 @@ class Backendless {
         return this.instance.put(`${path}/${columnName}`, column)
 
     }
+
     removeColumn(appId, table, column) {
         const columnName = column.name || column.columnName
         let path = tableColumnsUrl(appId, table)
@@ -349,9 +351,11 @@ class Backendless {
     addSecurityRole(appId, roleName) {
         return this.instance.put(`${appId}/console/security/roles/${roleName}`, {})
     }
+
     updateSecurityRole(appId, roleId, premission) {
         return this.instance.put(`${appId}/console/security/roles/permissions/${roleId}`, premission)
     }
+
     removeSecurityRole(appId, roleId) {
         return this.instance.delete(`${appId}/console/security/roles/${roleId}`)
     }
@@ -359,6 +363,7 @@ class Backendless {
     updateTablePermissions(appId, tableId, roleId, premissions) {
         return this.instance.put(`${appId}/console/security/data/${tableId}/roles/${roleId}`, premissions)
     }
+
     resetTablePermissions(appId, tableId, roleId) {
         return this.instance.delete(`${appId}/console/security/data/${tableId}/roles/${roleId}`)
     }
@@ -366,6 +371,7 @@ class Backendless {
     updateEndpointPermissions(appId, serviceId, roleId, premissions) {
         return this.instance.put(`${appId}/console/security/localservices/${serviceId}/roles/${roleId}`, premissions)
     }
+
     resetEndpointPermissions(appId, serviceId, roleId, operation) {
         return this.instance.delete(`${appId}/console/security/localservices/${serviceId}/roles/${roleId}/${operation}`)
     }
@@ -373,6 +379,7 @@ class Backendless {
     createRecord(appId, table, record) {
         return this.instance.post(`${appId}/console/data/${table}`, record)
     }
+
     deleteRecord(appId, table, record) {
         return this.instance.delete(`${appId}/console/data/tables/${table}/records`, { data: [record] })
     }
@@ -381,14 +388,46 @@ class Backendless {
         return this.instance.put(`${appId}/console/security/assignedroles`, { users, roles })
     }
 
+    static dump(app, path, verbose) {
+        const { ridOfIds, saveDataToFile } = Backendless
+
+        return saveDataToFile(ridOfIds(app), path, verbose)
+    }
+
+    static ridOfIds(app) {
+        const removeId = item => delete item.id
+        const removeRoleId = role => delete role.roleId
+        const removeColumnId = column => delete column.columnId
+        const removeRelationId = relation => {
+            delete relation.columnId
+            delete relation.fromTableId
+            delete relation.toTableId
+        }
+
+        app.tables.forEach(table => {
+            delete table.tableId;
+
+            (table.columns || []).forEach(removeColumnId);
+            (table.relations || []).forEach(removeRelationId);
+            (table.geoRelations || []).forEach(removeRelationId);
+            (table.roles || []).forEach(removeRoleId)
+        })
+
+        app.roles.forEach(removeRoleId)
+
+        app.services.forEach(service => {
+            removeId(service)
+            service.methods.forEach(removeId)
+        })
+
+        return app
+    }
+
     static saveDataToFile(data, path, verbose) {
-        return writeFile(path, JSON.stringify(data))
+        return writeFile(path, JSON.stringify(data, null, 2))
             .then(() => verbose && console.log(chalk.bold.green(`...... Schema is saved`)))
             .catch(e => console.error(e))
     }
-
-
-
 }
 
 module.exports = Backendless
